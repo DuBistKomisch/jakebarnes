ae775d2f6645d40180c6b418b59631a823339e14
Jake Barnes <jakerodneybarnes@gmail.com>
2019-04-10 23:44:17 +1000

    Click timer to toggle, add standing timer

--- public/js/twenty.js
+++ public/js/twenty.js
@@ -1,19 +1,32 @@
+const limits = {
+  twenty: {
+    go: 20 * 60,
+    stop: 20
+  },
+  stand: {
+    go: 2 * 60 * 60,
+    stop: 60 * 60
+  }
+};
+let mode = 'twenty';
+let stop = true;
+let count = 0;
+
 document.addEventListener('DOMContentLoaded', () => {
-  const body = document.getElementsByTagName('body')[0];
+  const fill = document.getElementById('fill');
   const text = document.getElementById('text');
-  let stop = true;
-  let count = 0;
+  const modeSelect = document.getElementById('mode');
 
   const tick = () => {
     if (count === 0) {
       if (stop) {
         stop = false;
-        count = 20 * 60;
-        body.className = 'go';
+        count = limits[mode].go;
+        fill.className = 'go';
       } else {
         stop = true;
-        count = 20;
-        body.className = 'stop';
+        count = limits[mode].stop;
+        fill.className = 'stop';
       }
     } else {
       count--;
@@ -22,6 +35,18 @@ document.addEventListener('DOMContentLoaded', () => {
     const seconds = (count % 60).toString().padStart(2, '0');
     text.innerText = `${minutes}:${seconds}`;
   };
+
+  fill.addEventListener('click', () => {
+    count = 0;
+    tick();
+  });
+
+  modeSelect.addEventListener('change', () => {
+    mode = modeSelect.value;
+    count = 0;
+    stop = true;
+    tick();
+  });
 
   setInterval(tick, 1000);
   tick();
