942999fa0dcf094d943a5827595786d80be98637
Jake Barnes <jakerodneybarnes@gmail.com>
2019-05-11 14:23:55 +1000

    Use constant for SECONDS_PER_YEAR

--- src/home.rs
+++ src/home.rs
@@ -3,6 +3,8 @@ use rocket::get;
 use rocket_contrib::templates::Template;
 use serde::Serialize;
 
+const SECONDS_PER_YEAR: i64 = 31_557_600; // 365.25 * 24 * 60 * 60;
+
 #[derive(Serialize)]
 struct HomeContext {
     age: i64
@@ -10,6 +12,6 @@ struct HomeContext {
 
 #[get("/")]
 pub fn get() -> Template {
-    let age = Local::today().signed_duration_since(Local.ymd(1992, 8, 19)).num_seconds() / 31557600;
+    let age = Local::today().signed_duration_since(Local.ymd(1992, 8, 19)).num_seconds() / SECONDS_PER_YEAR;
     Template::render("home", HomeContext { age })
 }
